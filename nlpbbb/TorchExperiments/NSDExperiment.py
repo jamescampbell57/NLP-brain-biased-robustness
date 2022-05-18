bugfixes including softmax fix

nlpbbb/TorchExperiments/NSDExperiment.py
@@ -39,8 +39,8 @@ class Experiment():
         # really you only want to build a model for an experiment object if it is the train experiment
         self.model = self.get_model()
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=config["experiment"]["lr"])
-        #num_iters = sum([len(dl) for dl in self.train_loaders])
-        #self.lr_scheduler = get_scheduler(name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=num_iters)
+        num_iters = sum([len(dl) for dl in self.train_loaders])
+        self.lr_scheduler = get_scheduler(name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=num_iters)
         self.lr_scheduler = None
         self.loss_function = torch.nn.MSELoss()
         

nlpbbb/TorchExperiments/YelpExperiment.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 
 # nlpbbb imports
 import nlpbbb as bbb
+from transformers import get_scheduler
 
 # misc imports
 import os
@@ -18,8 +19,8 @@ from nlpbbb.paths import PATHS
 class Experiment():
     
     def __init__(self, config):
-        self.train_datasets = [YelpDataset(ds, config["dataset"]) for ds in config["dataset"]["train_datasets"]]
-        self.val_datasets = [YelpDataset(ds, config["dataset"]) for ds in config["dataset"]["val_datasets"]]
+        self.train_datasets = [YelpDataset(ds, config["dataset"]["train_limit"], config["dataset"]) for ds in config["dataset"]["train_datasets"]]
+        self.val_datasets = [YelpDataset(ds, config["dataset"]["val_limit"], config["dataset"]) for ds in config["dataset"]["val_datasets"]]
         
         self.val_loaders = []
         for index, ds in enumerate(self.val_datasets):
@@ -36,8 +37,8 @@ class Experiment():
         # really you only want to build a model for an experiment object if it is the train experiment
         self.model = self.get_model(config["model"])
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=config["experiment"]["lr"])
-        #num_iters = sum([len(dl) for dl in self.train_loaders])
-        #self.lr_scheduler = get_scheduler(name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=num_iters)
+        num_iters = sum([len(dl) for dl in self.train_loaders])
+        self.lr_scheduler = get_scheduler(name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=num_iters)
         self.loss_function = torch.nn.MSELoss()
         self.lr_scheduler = None
         
@@ -64,7 +65,7 @@ class Experiment():
 
     
 class YelpDataset(Dataset):
-    def __init__(self, ds, dataset_config):
+    def __init__(self, ds, limit, dataset_config):
         data_path = f'{PATHS["root"]}/data/yelp'
         
         if not os.path.exists(os.path.join(data_path, f'{ds}.json')):
@@ -120,13 +121,13 @@ class YelpDataset(Dataset):
                 json.dump(chinese, f5)
             with open(os.path.join(data_path, 'italian.json'), 'w') as f6:
                 json.dump(italian, f6)
-            
+        
         lang_file = open(os.path.join(data_path, f'{ds}.json'))
         language = [json.loads(line) for line in lang_file] 
         lang_file.close()
         language = language[0]
         na = []
-        for i in language:
+        for i in language[:limit]:
             na.append({'text': i['text'], 'labels': F.one_hot((torch.tensor(i['stars']-1)).to(torch.int64), num_classes=5)})
         self.tokenized_data = na
         

nlpbbb/networks.py
@@ -38,7 +38,7 @@ class AmazonBERT(nn.Module):
             filtered_odict = change_all_keys(pre_odict)
             self.bert.load_state_dict(filtered_odict, strict=True)
         self.linear = nn.Linear(768, 5)
-        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
         
     def forward(self, x):
         #embeddings = self.tokenizer(x, return_tensors='pt', padding=True)
@@ -50,7 +50,7 @@ class AmazonBERT(nn.Module):
         #    return cls_representation
         #if self.sigmoid_bool:
         #    return self.sigmoid(pred)
-        return pred
+        return self.softmax(pred)
 
     
 class MNLIBert(nn.Module):
@@ -97,11 +97,7 @@ class SST2BERT(nn.Module):
         representations = self.bert(**x).last_hidden_state
         cls_representation = representations[:,0,:]
         pred = self.linear(cls_representation)
-        if self.return_CLS_representation:
-            return cls_representation
-        if self.sigmoid_bool:
-            return self.sigmoid(pred)
-        return pred
+        return self.sigmoid(pred)
     
     
 class STSBBERT(nn.Module):
@@ -134,15 +130,15 @@ class YelpBERT(nn.Module):
             filtered_odict = change_all_keys(pre_odict)
             self.bert.load_state_dict(filtered_odict, strict=True)
         self.linear = nn.Linear(768,5)
-        self.sigmoid = nn.Sigmoid()
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.softmax = nn.Softmax(dim=1)
     def forward(self, x):
         embeddings = self.tokenizer(x, return_tensors='pt', padding=True, truncation=True)
         embeddings.to(self.device)
         representations = self.bert(**embeddings).last_hidden_state
         cls_representation = representations[:,0,:]
         pred = self.linear(cls_representation)
-        return pred
+        return self.softmax(pred)
     
 
 class ReCoRDBERT(nn.Module):

nlpbbb/training_loops.py
@@ -42,8 +42,9 @@ def run_training_config(config):
             if config["experiment"]["experiment_type"] == "HarryPotter" or config["experiment"]["experiment_type"] == "NSD":
                 val_losses.append(val_loop(config["experiment"], exp, epoch, exp.val_loaders[0], device))
             else:
-                for val_loader in exp.val_loaders:
-                    val_losses.append(val_loop(config["experiment"], exp, epoch, val_loader, device))
+                if False:
+                    for val_loader in exp.val_loaders:
+                        val_losses.append(val_loop(config["experiment"], exp, epoch, val_loader, device))
             
         train_loss = train_loop(config["experiment"], exp, epoch, device)
         #save whenever you validate
@@ -55,7 +56,7 @@ def run_training_config(config):
                 else:
                     for i, val_name in enumerate(config["dataset"]["val_datasets"]):
                         wandb.log({val_name: val_losses[i]})
-                bbb.utils.save_model(exp, np.mean(val_losses), config, date, epoch)
+                #bbb.utils.save_model(exp, np.mean(val_losses), config, date, epoch)
 
         
 def train_loop(train_config, exp, epoch, device):