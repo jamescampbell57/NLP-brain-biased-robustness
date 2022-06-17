352754ab839eac221285447ebb84973453f29a5d
James Campbell <70195999+jamescampbell57@users.noreply.github.com>
2022-06-16 17:39:34 -0700

    path

--- evaluation/run_mnli.py
+++ evaluation/run_mnli.py
@@ -21,7 +21,7 @@ def single_run(batch_size, learning_rate):
     
     ################################################################
 
-    dataset_path = '/home/ubuntu/nlp-brain-biased-robustness/data/mnli'
+    dataset_path = '/home/ubuntu/NLP-brain-biased-robustness/data/mnli'
     data_path = dataset_path+'/multinli_1.0'
     if not os.path.exists(data_path):
         os.system('mkdir '+dataset_path)
