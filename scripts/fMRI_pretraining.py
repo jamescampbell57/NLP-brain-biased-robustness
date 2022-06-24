e2d2957b33395b089e6b9caa4aa043eb7235230c
james <jgc239@cornell.edu>
2022-06-24 01:25:53 +0000

    NSD pretraining

--- scripts/fMRI_pretraining.py
+++ scripts/fMRI_pretraining.py
@@ -7,6 +7,14 @@ from transformers import BertModel
 import numpy as np
 from scipy.io import loadmat
 import os
+import random
+
+import scipy.io
+import matplotlib.pyplot as plt
+import h5py
+from pycocotools.coco import COCO
+import time
+import csv
 
 
 class HarryPotterDataset(Dataset):
@@ -80,15 +88,92 @@ class HarryPotterDataset(Dataset):
 
     def __len__(self):
         return len(self.fmri_data)
+    
+    
+class NSD(Dataset):
+    def __init__(self, voxels=False):
+        data_path = '/home/ubuntu/NLP-brain-biased-robustness/NSD/'
+
+        coco3 = COCO(data_path+'annotations/captions_train2017.json')
+        coco4 = COCO(data_path+'annotations/captions_val2017.json')
+
+        def load_csv(csv_file):
+            file = open(csv_file)
+            csvreader = csv.reader(file)
+            header = next(csvreader)
+            rows = []
+            for row in csvreader:
+                rows.append(row)
+            file.close()
+            return rows
+
+        nsd_to_coco = load_csv(data_path+'nsd_stim_info_merged.csv')
+        exp_design = scipy.io.loadmat(data_path+'nsd_expdesign.mat')
+        ordering = exp_design['masterordering'].flatten() - 1 #fix indexing
+
+        data_size = 22500 #trials[subject-1] #can use more than 22500 trials if seems promising
+        ordering_data = ordering[:data_size]
+        subjectim = exp_design['subjectim'] - 1
+
+        def index_to_captions(my_index, subject):
+            index = ordering_data[my_index]
+            nsd_id = subjectim[subject-1,index]
+            coco_id = nsd_to_coco[nsd_id][1]
+            if int(nsd_id) < 2950:
+                annotation_ids = coco4.getAnnIds(int(coco_id))
+                annotations = coco4.loadAnns(annotation_ids)
+            else:
+                annotation_ids = coco3.getAnnIds(int(coco_id))
+                annotations = coco3.loadAnns(annotation_ids)
+            captions = [item['caption'] for item in annotations]
+            return captions
+
+        NSD_fmri_parcellated = np.empty((22500,23,8))
+        for subject in range(8):
+            X = scipy.io.loadmat(data_path+'X'+str(subject+1)+'.mat')
+            NSD_fmri_parcellated[:,:,subject] = X['X']
 
 
-dataset_1 = HarryPotterDataset(1)
-n_rows = len(dataset_1)
-train_dataset = dataset_1[:int(.8*n_rows)]
-val_dataset = dataset_1[int(.9*n_rows):]
+        self.fmri_data = []
+        for subject in range(4):
+            for my_index in range(22000):
+                descriptions = index_to_captions(my_index, 1)
+                brain_vec = NSD_fmri_parcellated[my_index,:,0]
+                for description in descriptions[:3]:
+                    example = (description, brain_vec)
+                    self.fmri_data.append(example)
 
-train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-test_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=True)
+        if voxels:
+            #implement separate function for voxel-wise data
+            assert(False)
+
+        
+    def __getitem__(self, idx):
+        return self.fmri_data[idx]
+
+    def __len__(self):
+        return len(self.fmri_data)
+
+                          
+     
+dataset_2 = NSD()
+random.shuffle(dataset_2)
+split_place = int(.8*len(dataset_2))
+train_dataset = dataset_2[:split_place]
+val_dataset = dataset_2[split_place:]
+
+
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+
+
+#dataset_1 = HarryPotterDataset(1)
+#n_rows = len(dataset_1)
+#train_dataset = dataset_1[:int(.8*n_rows)]
+#val_dataset = dataset_1[int(.9*n_rows):]
+
+#train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+#test_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=True)
 
 
 class BrainBiasedBERT(nn.Module):
@@ -113,11 +198,11 @@ from tqdm.auto import tqdm
 import wandb
 
 run = wandb.init(project="fMRI pretraining", entity="nlp-brain-biased-robustness")
-wandb.run.name = 'subject 1 harry potter'
+wandb.run.name = 'NSD parcellated subjects 1-4'
 wandb.config = {
   "learning_rate": 1e-5,
   "epochs": 40,
-  "batch_size": 8
+  "batch_size": 16
 }
 
 def evaluate(model, dataloader):
@@ -163,8 +248,8 @@ def train(model, dataloader, num_epochs=40):
         wandb.log({"training loss": loss.item()})
         wandb.log({"val loss": val_loss})
         save_dir = '/home/ubuntu/NLP-brain-biased-robustness/state_dicts'
-        if epoch % 20 == 0 or epoch == 5 or epoch == 3:
-            torch.save(model.state_dict(), os.path.join(save_dir, f'cereberto_epoch_{epoch}'))
+        if epoch % 10 == 0 or epoch == 5 or epoch == 3 or epoch ==1 or epoch ==2 or epoch==4:
+            torch.save(model.state_dict(), os.path.join(save_dir, f'cereberto_NSD_parc_epoch_{epoch}'))
         
-model = BrainBiasedBERT()
+model = BrainBiasedBERT(num_voxels=23)
 train(model, train_dataloader)
